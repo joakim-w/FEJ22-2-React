b87b5f49f96bc8a6b4aa0be8470ae90d489a993e
Joakim <wahlstrom.joakim@gmail.com>
2022-12-27 10:52:35 +0100

    lektion 1

--- Lektion-1/1-props/src/App.js
+++ Lektion-1/1-props/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   
+  const title = 'Jockes hemsida'
+  const text = 'hejsan'
+
   return (
     <>
-      <div className="App">
-      hej
+    <Navbar pageTitle={title} prop1='1' prop2='2' />
+      <div className="App"  >
+      {text}
       </div>
       <div></div>
     </>
